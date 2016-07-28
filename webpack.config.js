f1e4c1852f8ddf2520ee50536f6fa831059df16f
OoogleBoogle <simattfield@gmail.com>
2016-07-28 10:46:33 -0700

    new build

--- webpack.config.js
+++ webpack.config.js
@@ -11,9 +11,7 @@ plugins.push(new ExtractTextPlugin('style.css'));
 plugins.push(new HtmlWebpackPlugin({
     template: 'src/index.ejs'
 }));
-plugins.push(new webpack.DefinePlugin({
-    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
-}));
+
 module.exports = {
     entry: path.resolve(__dirname, packageData.main),
     output: {
